8a2ae73ee69284d703e377418a744c4070205b21
Kristjan-O-Ragnarsson <kristjan@ragnarsson.im>
2019-01-26 16:38:42 +0000

    Update runa.go

--- runa.go
+++ runa.go
@@ -8,14 +8,16 @@ func main() {
 	fmt.Println("runa verk efni 4")
 	var n int32
 	fmt.Scanln(&n)
-	x := summa(n)
-	fmt.Println(x)
+	summa(n)
 }
 
 func summa(n int32) int32{
 	if n == 1{
+		fmt.Printf("1 ")
 		return 1
 	} else {
-		return summa(n -1) +  n
+		su := summa(n -1) +  n
+		fmt.Printf("%v ", su)
+		return su
 	}
 }
